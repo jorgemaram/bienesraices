9df678d729c74e2b8009935a0ef6a4951f52ba30
Jorge Martin <jorgemaram@gmail.com>
2021-03-12 13:17:04 +0100

    creation of hook useInicio and bring data to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
 import Layout from '../components/layout';
-
+import useInicio from '../hooks/useInicio';
 
 const Index = () => {
+
+  const inicio = useInicio();
+  
+  const { nombre, contenido, imagen } = inicio[0];
+
   return (
     <Layout>
-      <h1>Index</h1>
+      <h1>{nombre}</h1>
+      <p>{contenido}</p>
     </Layout>
    );
 }
