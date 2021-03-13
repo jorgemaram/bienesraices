a006e53c29592e63eb08a7c1e8db955077e0e6a7
Jorge Martin <jorgemaram@gmail.com>
2021-03-13 19:32:05 +0100

    filtered results depending on type of property

--- src/components/listadoPropiedades.js
+++ src/components/listadoPropiedades.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { css } from '@emotion/react';
 import usePropiedades from '../hooks/usePropiedades';
+import useFiltro from '../hooks/useFiltro';
 import PropiedadPreview from './propiedadPreview';
 import * as  listadoPropiedesCSS from '../css/listadoPropiedades.module.css';
 
@@ -9,24 +10,36 @@ import * as  listadoPropiedesCSS from '../css/listadoPropiedades.module.css';
 const ListadoPropiedades = () => {
 
     const resultado = usePropiedades();
-    const [propiedades, guardarPropiedades] = useState([]);
+    const [propiedades] = useState(resultado);
+    const [filtradas, guardarFiltradas] = useState([]);
+
+    //filtrado de propiedades
+
+    const { categoria, FiltroUI } = useFiltro()
+
 
     useEffect(() => {
-        guardarPropiedades(resultado)
-    }, [])
+        if (categoria) {
+            const filtro = propiedades.filter(propiedad => propiedad.categoria.nombre === categoria);
+            guardarFiltradas(filtro)
+        } else {
+            guardarFiltradas(propiedades)
+        }
+    }, [categoria])
 
     return (
         <>
             <h2 css={css`
                 margin-top: 5rem;
             `}>Nuestras propiedades</h2>
+            {FiltroUI()}
             <ul className={listadoPropiedesCSS.propiedades}>
-                {propiedades.map(propiedad => (
+                {filtradas.map(propiedad => (
                     <PropiedadPreview
                         key={propiedad.id}
                         propiedad={propiedad}
                     />
-            ))}
+                ))}
             </ul>
         </>
     );
