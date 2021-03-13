375773c249edb40d1567e4edc51f41e93a85260a
Jorge Martin <jorgemaram@gmail.com>
2021-03-13 17:55:38 +0100

    diplay info in card for houses

--- src/components/listadoPropiedades.js
+++ src/components/listadoPropiedades.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { css } from '@emotion/react';
 import usePropiedades from '../hooks/usePropiedades';
 import PropiedadPreview from './propiedadPreview';
+import * as  listadoPropiedesCSS from '../css/listadoPropiedades.module.css';
+
 
 
 const ListadoPropiedades = () => {
@@ -18,7 +20,7 @@ const ListadoPropiedades = () => {
             <h2 css={css`
                 margin-top: 5rem;
             `}>Nuestras propiedades</h2>
-            <ul>
+            <ul className={listadoPropiedesCSS.propiedades}>
                 {propiedades.map(propiedad => (
                     <PropiedadPreview
                         key={propiedad.id}
